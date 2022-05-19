a5097de8af8c75e4bccd85a59b63f7993d785d1f
docherak <docherak@protonmail.ch>
2022-05-20 00:34:02 +0200

    Simplify script, fix bugs
    Fixed incorrect even listener. Updated styles. Simplified functions

--- scripts.js
+++ scripts.js
@@ -1,35 +1,42 @@
+// Load buttons and play:
+
+// sem hodit if na 5 bodu a dat disable + reset option (asi pres fci)
+const btns = document.querySelectorAll('button');
+btns.forEach(btn => btn.addEventListener('click', playerPlay));
+
+// Functions:
 function computerPlay() {
-  const psr = ["Paper", "Scissors", "Rock"];
-  let randIndex = Math.floor(Math.random() * 3);
+  const psr = ["paper", "scissors", "rock", "lizard", "spock"];
+  let randIndex = Math.floor(Math.random() * 5);
   return psr[randIndex];
 }
 
-//let computerSelection = computerPlay().toLowerCase();
-//console.log(computerSelection);
+function playerPlay(e) {
+  let playerSelection = e.target.textContent.toLowerCase();
+  singleRound(playerSelection, computerPlay());
+}
 
-//let playerSelection = prompt("Paper/Scissors/Rock?").toLowerCase();
-//console.log(playerSelection);
+// dat disable buttons po 5 kolech
 
 function singleRound(playerSelection, computerSelection) {
+  console.log(computerSelection);
+  console.log(playerSelection);
   if (playerSelection == computerSelection) {
     alert("Tie");
     return 0;
-  } else if ((playerSelection == "paper") & (computerSelection == "rock")) {
+  } else if (((playerSelection == "scissors") & (computerSelection == "paper")) ||
+      ((playerSelection == "paper") & (computerSelection == "rock")) ||
+      ((playerSelection == "rock") & (computerSelection == "lizard")) ||
+      ((playerSelection == "lizard") & (computerSelection == "spock")) ||
+      ((playerSelection == "spock") & (computerSelection == "scissors")) ||
+      ((playerSelection == "scissors") & (computerSelection == "lizard")) ||
+      ((playerSelection == "lizard") & (computerSelection == "paper")) ||
+      ((playerSelection == "paper") & (computerSelection == "spock")) ||
+      ((playerSelection == "spock") & (computerSelection == "rock")) ||
+      ((playerSelection == "rock") & (computerSelection == "scissors"))) {
     alert("Point for player");
     return 1;
-  } else if ((playerSelection == "paper") & (computerSelection == "scissors")) {
-    alert("Point for the AI");
-    return 2;
-  } else if ((playerSelection == "scissors") & (computerSelection == "paper")) {
-    alert("Point for player");
-    return 1
-  } else if ((playerSelection == "scissors") & (computerSelection == "rock")) {
-    alert("Point for the AI");
-    return 2;
-  } else if ((playerSelection == "rock") & (computerSelection == "scissors")) {
-    alert("Point for player");
-    return 1;
-  } else if ((playerSelection == "rock") & (computerSelection == "paper")) {
+  } else {
     alert("Point for the AI");
     return 2;
   }
@@ -38,8 +45,10 @@ function singleRound(playerSelection, computerSelection) {
 function game() {
   let score = [0,0];
   while (score[0] < 5) {
-    let computerSelection = computerPlay().toLowerCase();
-    let playerSelection = prompt("Paper/Scissors/Rock?").toLowerCase();
+    let computerSelection = computerPlay();
+
+  //  let playerSelection = prompt("Paper/Scissors/Rock?").toLowerCase();
+
     let result = singleRound(playerSelection, computerSelection);
     if (result == 1) {
       score[0]++;
@@ -53,4 +62,11 @@ function game() {
   alert(`Final score >> Player: ${score[0]}, AI: ${score[1]}.`)
 }
 
-game();
+
+//for (btn of btns) {
+//  console.log(btn.textContent);
+//  btn.addEventListener("click", function () {singleRound(btn.textContent, computerSelection)});
+//}
+
+
+//game();
